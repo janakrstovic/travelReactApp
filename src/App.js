f3c39004056d1b42f92b9c303ff06940610f488e
janakrstovic <57789440+janakrstovic@users.noreply.github.com>
2021-06-26 19:12:10 +0200

    sredjivanje

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router,Switch} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+   <Header />
+    <Route exact to="/">
+    <Showcase/>
+    </Route>
+
+    <Route to="/login">
+    <Login/>
+    </Route>
+
+    <Route to="/signup">
+    <SignUp/>
+    </Route>
+     
+    <Footer/> 
+    </Router>
   );
 }
 
